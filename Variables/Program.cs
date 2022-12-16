4128530327c7132a6555c98a0652185673a10a15
Gunjan <gunjangoyal1911@gmail.com>
2022-12-16 17:16:35 +0100

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 5;
+            var number = 2;
+            var character= 'g';
+            var totalPrice = 72.34f;
+            var name = "Gunjan Goyal";
+            var isworking= false;
+
+            Console.WriteLine(count);
+            Console.WriteLine(number);
+            Console.WriteLine(character);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(name);
+            Console.WriteLine(isworking);
+
+        }
+    }
+}
